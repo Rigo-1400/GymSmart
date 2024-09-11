423dfb808419192cd3ba63aa835fbd2c7cf9cb8d
rigo <rodrigorodriguez1400@gmail.com>
2024-09-10 19:32:04 -0700

    Added working pages

--- app/src/main/java/com/example/anew/MainActivity.java
+++ app/src/main/java/com/example/anew/MainActivity.java
@@ -7,22 +7,55 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.widget.Button;
+import android.view.View;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        btn = (Button)findViewById(R.id.button_1);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(MainActivity.this,upper.class);  //This is button code for UPPER muscle group
+            startActivity(intent);
+            }
+        });
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        btn = (Button)findViewById(R.id.button2);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(MainActivity.this,lower.class);  //This is button code for LOWER muscle group
+                startActivity(intent);
+            }
+        });
+
+
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
+
+
         });
-    }
-    public void onClick(){
 
     }
+
 }
 
---
+++ app/src/main/java/com/example/anew/lower.java
@@ -0,0 +1,13 @@
+package com.example.anew;
+
+import android.app.Activity;
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+
+public class lower extends MainActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lower_muscle);
+    }
+}
