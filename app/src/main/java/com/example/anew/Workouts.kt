6c9e18ad26444b77140814c1f93cff42cb5c8ca8
Elias Dandouch <elias_dandouch1@my.vcccd.edu>
2024-09-18 22:43:58 -0700

    Added Workout.kt & Workouts.kt

---
+++ app/src/main/java/com/example/anew/Workouts.kt
@@ -0,0 +1,44 @@
+package com.example.anew
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+var workouts = arrayOf(
+    "Chest",
+    "Shoulders",
+    "Biceps",
+    "Triceps",
+);
+
+@Composable
+fun Workouts(navController: NavController) {
+
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding),
+            verticalArrangement = Arrangement.spacedBy(8.dp) // Add spacing between items
+        ) {
+            workouts.forEach { workoutName ->
+                Button(
+                    onClick = { navController.navigate("workout/$workoutName") },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp) // Adjust padding for better visual appearance
+                ) {
+                    Text(workoutName);
+                }
+            }
+        }
+    }
+}
