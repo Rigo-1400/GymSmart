97a1bbd348250710cb0fba5c8088bd26b1e49e0a
Elias Dandouch <elias_dandouch1@my.vcccd.edu>
2024-09-20 19:53:12 -0700

    Added a SearchBar (STILL NEEDS TO BE FULLY IMPLEMENTED)

--- app/src/main/java/com/example/anew/Workouts.kt
+++ app/src/main/java/com/example/anew/Workouts.kt
@@ -1,21 +1,38 @@
 package com.example.anew
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 // TODO: Add the ability to take in the Workouts parameter that will be an interface/object
 fun Workouts(navController: NavController, workouts: Array<String>) {
+
+    // Creating two State variables
+    var text by remember { mutableStateOf("") }
+    var active by remember { mutableStateOf(false) }
 
     Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
         Column(
@@ -24,6 +41,22 @@ fun Workouts(navController: NavController, workouts: Array<String>) {
                 .padding(innerPadding),
             verticalArrangement = Arrangement.spacedBy(8.dp) // Add spacing between items
         ) {
+            SearchBar(modifier = Modifier.fillMaxWidth(), query = text, onQueryChange = { text = it }, onSearch = { active = false }, active = active, onActiveChange = { active = it; }, placeholder = { Text("Search for workout") }, leadingIcon = { Icon(
+                Icons.Default.Search, "Search Icon") }, trailingIcon = {
+                    if(active) {
+                        Icon(modifier = Modifier.clickable {
+                            if (text.isNotEmpty()) {
+                                text = ""
+                            } else active = false
+                        }, imageVector = Icons.Default.Close, contentDescription = "Close Icon")
+                    }
+            }) {
+                workouts.forEach {
+                    Row(modifier = Modifier.padding(all = 14.dp)) {
+                        Text(text = it)
+                    }
+                }
+            }
             workouts.forEach { workoutName ->
                 Button(
                     onClick = { navController.navigate("workout/$workoutName") },
