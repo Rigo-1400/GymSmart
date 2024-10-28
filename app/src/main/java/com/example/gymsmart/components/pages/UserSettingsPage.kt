b34469055d8567cd3eac29ee43dcc6f5e1d12dc7
Duc Le <76507073+Sonny-77-98@users.noreply.github.com>
2024-10-28 14:20:26 -0700

    adding back button on action bar

--- app/src/main/java/com/example/gymsmart/components/pages/UserSettingsPage.kt
+++ app/src/main/java/com/example/gymsmart/components/pages/UserSettingsPage.kt
@@ -2,7 +2,6 @@ package com.example.gymsmart.components.pages
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.icons.Icons
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
